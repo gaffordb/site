3114f7e5e5d2882f5b45a467d66a387a28852ed7
gaffordb <gaffordb@grinnell.edu>
2020-02-19 22:31:57 -0600

    HW4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
+require 'open-uri'
+require 'json'
+
 module ApplicationHelper
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
@@ -8,4 +11,25 @@ module ApplicationHelper
       page_title + " | " + base_title
     end
   end
-end
+
+  def get_quote()
+    if false
+    begin
+    content = open("https://quotes.rest/qod?language=en",
+                      "X-TheySaidSo-Api-Secret" => "shoot")
+    resp_err = false
+    rescue OpenURI::HTTPError
+      resp_err = true
+      quote = "No more quotes today, sorry! There will be more tomorrow..."
+    unless resp_err
+      data = JSON.parse(content)['contents']
+      quote = data['quotes']['quote'] + '\n' +
+          data['quotes']['author']
+    end
+    end
+    end
+    quote = "No more quotes today, sorry! There will be more tomorrow..."
+    junk = "Also, here's a random number: #{rand(100)}"
+    quote + junk
+    end
+  end
